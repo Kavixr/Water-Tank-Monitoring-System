f157b5fd5d6586881f27354a9e0bfa1c692b2872
Kavindu Rajapaksha <kavindurajapaksha848@gmail.com>
2025-10-05 12:44:38 +0530

    import the need parts

---
+++ WaterTank.java
@@ -0,0 +1,13 @@
+import java.awt.FlowLayout;
+import java.awt.Font;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JSlider;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+
+public class WaterTank {
+    
+}
